f3a8b3884955d3546af35d8eb989df122dcbe74f
LeoRiether <leonardoalves8217@gmail.com>
2021-01-24 12:16:59 -0300

    test

--- api/main.rs
+++ api/main.rs
@@ -1,21 +1,43 @@
 use http::StatusCode;
 use now_lambda::{error::NowError, lambda, IntoResponse, Request, Response};
 use rand::prelude::*;
+use serde_json::json;
 use std::error::Error;
 
+// Parses and executes the text sent by a user, returning the response RNGesus
+// sent from the heavens
+fn execute(text: &str) -> String {
+    "Your bot code works perfectly".into()
+}
+
 fn handler(req: Request) -> Result<impl IntoResponse, NowError> {
-    // let body = match req.body() {
-    //     Binary(data) =>
-    //     _ => return Err(NowError::new("Request body is not in binary format"));
-    // }
-    // let body = deserialize(req);
-    println!("body = {:?}", req.body());
+    let body: serde_json::Value = match req.body() {
+        now_lambda::Body::Binary(data) => serde_json::from_slice(data).expect("Incorrectly formatted json"),
+        _ => return Err(NowError::new("Request body is not in binary format"))
+    };
+
+    let text = match &body["message"]["text"] {
+        serde_json::Value::String(x) => x,
+        _ => return Err(NowError::new("body.message.text does not exist")),
+    };
+
+    let chat_id = match &body["chad"]["id"] {
+        serde_json::Value::Number(x) => x,
+        _ => return Err(NowError::new("body.chat.id does not exist")),
+    };
+
+    let response_json = json!({
+        "method": "sendMessage",
+        "chat_id": chat_id,
+        "text": execute(&text),
+    });
+
+    println!("response = {:#?}", response_json);
 
     let response = Response::builder()
         .status(StatusCode::OK)
-        .header("Content-Type", "text/plain")
-        // .body("user endpoint")
-        .body(format!("body = {:?}", req.body()))
+        .header("Content-Type", "application/json")
+        .body(response_json.to_string())
         .expect("Internal Server Error");
 
     Ok(response)
