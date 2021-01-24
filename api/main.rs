90bb523acd638a2ade6484932ba64d0319fe59f5
LeoRiether <leonardoalves8217@gmail.com>
2021-01-24 11:07:57 -0300

    deserialize

--- api/main.rs
+++ api/main.rs
@@ -3,8 +3,17 @@ use now_lambda::{error::NowError, lambda, IntoResponse, Request, Response};
 use rand::prelude::*;
 use std::error::Error;
 
+fn deserialize<T>(req: Request<Vec<u8>>) -> serde_json::Result<Request<T>>
+    where for<'de> T: serde::de::Deserialize<'de>,
+{
+    let (parts, body) = req.into_parts();
+    let body = serde_json::from_slice(&body)?;
+    Ok(Request::from_parts(parts, body))
+}
+
 fn handler(req: Request) -> Result<impl IntoResponse, NowError> {
-    println!("req = {:?}", req);
+    let body = deserialize(req);
+    println!("body = {:?}", body);
 
     let response = Response::builder()
         .status(StatusCode::OK)
